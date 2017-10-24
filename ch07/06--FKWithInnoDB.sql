15787a38542111a93093b45d2f9ceebcd3050a99
DESKTOP-6POJJMP\kiky <huafeihua123xxx@gmail.com>
2017-10-24 19:33:18 +0800

    打开/关闭外键约束检查

--- ch07/06--FKWithInnoDB.sql
+++ ch07/06--FKWithInnoDB.sql
@@ -10,3 +10,9 @@ DELETE FROM country WHERE country_id=1;
 #尝试更新有外键关联的父表记录
 #——> 关联的子表记录也被更新
 UPDATE country SET country_id=10000 WHERE country_id=1;
+
+#打开外键约束检查
+SET FOREIGN_KEY_CHECKS=1;
+
+#关闭外键约束检查
+SET FOREIGN_KEY_CHECKS=0;

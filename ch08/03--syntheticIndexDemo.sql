1594a0f89fee4ff8eb29dc8609e5d05cf62bd361
DESKTOP-6POJJMP\kiky <huafeihua123xxx@gmail.com>
2017-10-25 18:58:55 +0800

    合成索引

---
+++ ch08/03--syntheticIndexDemo.sql
@@ -0,0 +1,23 @@
+####
+#合成索引
+###
+#删除表 t
+DROP TABLE IF EXISTS t;
+
+#创建表 t
+#其具备合成索引字段，以提高查询效率
+CREATE TABLE t(
+	id VARCHAR(100),
+	context BLOB,
+	hash_value VARCHAR(40)
+);
+
+#向表 t 中插入数据
+INSERT INTO t
+	VALUES('1',REPEAT('beijing',2),MD5(context)),
+		('2',REPEAT('beijing',2),MD5(context)),
+		('3',REPEAT('beijing 2008',2),MD5(context));
+
+
+#查询合成索引字段
+SELECT * FROM t WHERE hash_value=MD5(REPEAT('beijing 2008',2));

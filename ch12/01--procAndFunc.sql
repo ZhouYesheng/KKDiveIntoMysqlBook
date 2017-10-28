0c7965e90613199a1c2ace2aacbaecffee735b37
DESKTOP-6POJJMP\kiky <huafeihua123xxx@gmail.com>
2017-10-28 10:58:19 +0800

    以不同安全权限创建存储过程

--- ch12/01--procAndFunc.sql
+++ ch12/01--procAndFunc.sql
@@ -14,3 +14,33 @@ BEGIN
 	SELECT FOUND_ROW() INTO p_film_count;
 END$$
 DELIMITER ;
+
+#创建存储过程 film_in_stock_definer
+#以创建者权限执行
+DELIMITER $$
+CREATE PROCEDURE film_in_stock_definer(IN p_film_id INT
+	,IN p_store_id INT,OUT p_film_count INT)
+	SQL SECURITY DEFINER
+BEGIN
+	SELECT invenory_id FROM inventory
+		WHERE film_id=p_film_id	AND store_id=p_store_id
+			AND inventory_in_stock(invenory_id);
+
+	SELECT FOUND_ROW() INTO p_film_count;
+END$$
+DELIMITER ;
+
+#创建存储过程 film_in_stock_invoker
+#以调用者权限执行
+DELIMITER $$
+CREATE PROCEDURE film_in_stock_invoker(IN p_film_id INT
+	,IN p_store_id INT,OUT p_film_count INT)
+	SQL SECURITY INVOKER
+BEGIN
+	SELECT invenory_id FROM inventory
+		WHERE film_id=p_film_id	AND store_id=p_store_id
+			AND inventory_in_stock(invenory_id);
+
+	SELECT FOUND_ROW() INTO p_film_count;
+END$$
+DELIMITER ;

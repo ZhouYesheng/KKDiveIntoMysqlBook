4f28a3c2027aaf85af11e3f9237d11d59f0ace6f
DESKTOP-6POJJMP\kiky <huafeihua123xxx@gmail.com>
2017-10-31 07:30:14 +0800

    线性哈希分区

--- ch17/06--partiedByHash.sql
+++ ch17/06--partiedByHash.sql
@@ -19,3 +19,20 @@ SELECT * FROM emp;
 
 #插入测试数据 emp
 INSERT INTO emp VALUES(1,'Tom','2010-10-10','9999-12-31','CLERK',234);
+
+
+#创建分区表 emp
+#——》线性哈希分区
+DROP TABLE IF EXISTS emp;
+CREATE TABLE emp(
+	id INT NOT NULL,
+	ename VARCHAR(30),
+	hired DATE NOT NULL DEFAULT '1970-01-01',
+	separated DATE NOT NULL DEFAULT '9999-12-31',
+	job VARCHAR(30) NOT NULL,
+	store_id INT NOT NULL
+)
+PARTITION BY LINEAR HASH(store_id) PARTITIONS 4;
+
+#查看表 emp
+SELECT * FROM emp;

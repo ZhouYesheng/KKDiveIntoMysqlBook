005ec98687cadb563993e3095bf791c7c8c98440
DESKTOP-6POJJMP\kiky <huafeihua123xxx@gmail.com>
2017-10-31 08:31:14 +0800

    LIST 分区定义不包含 NULL 值时，插入 NULL 值会报错

--- ch17/09--nullWithPartied.sql
+++ ch17/09--nullWithPartied.sql
@@ -21,3 +21,25 @@ SELECT * FROM tb_range;
 
 #插入 NULL 值
 INSERT INTO tb_range VALUES(NULL,'NULL');
+
+#创建表 tb_list
+#LIST 分区定义不包含 NULL 值时，插入 NULL 值会报错
+DROP TABLE IF EXISTS tb_list;
+CREATE TABLE tb_list(
+	id INT,
+	name VARCHAR(5)
+)
+PARTITION BY LIST(id)
+(
+	PARTITION p1 VALUES IN (0),
+	PARTITION p2 VALUES IN (1)
+);
+
+#查询表 tb_list
+SELECT * FROM tb_list;
+
+#插入 NULL 值会报错
+INSERT INTO tb_list VALUES(NULL,'NULL');
+
+
+
